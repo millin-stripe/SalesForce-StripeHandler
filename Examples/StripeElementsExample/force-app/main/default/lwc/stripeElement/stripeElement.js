6abe6aa253d4f60a67587b27bf768131f0ea0de6
millin-stripe <87815854+millin-stripe@users.noreply.github.com>
2021-11-09 14:38:50 -0500

    Example 1

---
+++ Examples/StripeElementsExample/force-app/main/default/lwc/stripeElement/stripeElement.js
@@ -0,0 +1,27 @@
+import { LightningElement, track, api } from 'lwc';
+import makeGetCallout from '@salesforce/apex/StripeAPIHandler.makeGetCallout';
+export default class PaymentElements extends LightningElement {
+    //@track siteclientSecret;
+    @api siteclientSecret;
+    @api isReloaded = false;
+    async renderedCallback(){
+        var amount = 4000;
+        var currency = 'usd';
+        var paymentMethodone = 'card';
+        var paymentMethodtwo = 'afterpay_clearpay';
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("amount", amount);
+        urlencoded.append("currency", currency);
+        urlencoded.append("payment_method_types[0]", paymentMethodone);
+        urlencoded.append("payment_method_types[1]", paymentMethodtwo);
+        var method = 'POST';
+        var url = 'https://api.stripe.com//v1/payment_intents'
+        let presult = await makeGetCallout({urlencoded:urlencoded.toString(),url:url, method:method});
+        var result = JSON.parse(presult);
+        if (this.siteclientSecret == undefined){
+            this.siteclientSecret = '/apex/StripeElement?clientSecret=' + result.client_secret;
+            console.log(this.siteclientSecret);
+        } 
+    }
+    
+}
