1a5b3421d9320fc6c4cac9665ec9f519cc920c33
millin-stripe <87815854+millin-stripe@users.noreply.github.com>
2021-11-11 13:03:16 -0500

    Example 1

--- Examples/StripeElementsExample/force-app/main/default/lwc/stripeElement/stripeElement.js
+++ Examples/StripeElementsExample/force-app/main/default/lwc/stripeElement/stripeElement.js
@@ -1,24 +1,39 @@
 import { LightningElement, track, api } from 'lwc';
 import makeGetCallout from '@salesforce/apex/StripeAPIHandler.makeGetCallout';
 export default class PaymentElements extends LightningElement {
-    //@track siteclientSecret;
+
     @api siteclientSecret;
     @api isReloaded = false;
+//This starts the url call back into stripe.
     async renderedCallback(){
-        var amount = 4000;
-        var currency = 'usd';
-        var paymentMethodone = 'card';
-        var paymentMethodtwo = 'afterpay_clearpay';
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("amount", amount);
-        urlencoded.append("currency", currency);
-        urlencoded.append("payment_method_types[0]", paymentMethodone);
-        urlencoded.append("payment_method_types[1]", paymentMethodtwo);
-        var method = 'POST';
-        var url = 'https://api.stripe.com//v1/payment_intents'
-        let presult = await makeGetCallout({urlencoded:urlencoded.toString(),url:url, method:method});
-        var result = JSON.parse(presult);
+        //check to see if this iframe url is set for the visualforce page. Due to security restrictions around external JS loads in SF
+        //we have to do a Visualforce page and not directly with a lwc
         if (this.siteclientSecret == undefined){
+            //Vaiables you can set
+            var amount = 4000;
+            var currency = 'usd';
+            var paymentMethodone = 'card';
+
+            //Start calling the Stripe API Handler Class
+            //Define url Parameters if needed
+            var urlencoded = new URLSearchParams();
+            urlencoded.append("amount", amount);
+            urlencoded.append("currency", currency);
+            urlencoded.append("payment_method_types[0]", paymentMethodone);
+
+            //Define URL Method
+            var method = 'POST';
+
+            //Define URL to call in stripe
+            var url = 'https://api.stripe.com//v1/payment_intents'
+
+            //Call the Stripe API Handler class. this returns a promise with a string body that is JSON
+            let presult = await makeGetCallout({urlencoded:urlencoded.toString(),url:url, method:method});
+
+            //Take the text return and make it a JSON object
+            var result = JSON.parse(presult);
+            
+            //This creates the url for the iframe for the visualforce page StripeElement. 
             this.siteclientSecret = '/apex/StripeElement?clientSecret=' + result.client_secret;
             console.log(this.siteclientSecret);
         } 
